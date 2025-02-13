b07faa5359cc218148f4f7bfd8acf4c9f3986a76
dhlittl <dhlittl@clemson.edu>
2025-02-13 14:16:42 -0500

    Initial commit

---
+++ pages/app/dashboard/driver/driverDashboard.js
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+    return (
+      <div>
+        <h1>Welcome to the Dashboard</h1>
+        <p>This is where users can see their dashboard info.</p>
+      </div>
+    );
+  }
